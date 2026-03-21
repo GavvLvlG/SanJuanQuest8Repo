46e25c08b4d0da40f6bfd628da859f5ce31f0ba1
GavvLvlG <antonsanjuanjr@gmail.com>
2026-03-21 10:05:41 +0800

    Particle Control Script

--- Assets/Script/Particle Controller.cs
+++ Assets/Script/Particle Controller.cs
@@ -2,15 +2,63 @@ using UnityEngine;
 
 public class ParticleController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private ParticleSystem particleSystem2D;
+
+    [Header("Settings")]
+    [Tooltip("If true, the particle effect will destroy itself after finishing.")]
+    public bool autoDestroy = true;
+
+    [Tooltip("Delay before destroying the GameObject after particles stop.")]
+    public float destroyDelay = 0.5f;
+
+    void Awake()
     {
-        
+        // Get the ParticleSystem component
+        particleSystem2D = GetComponent<ParticleSystem>();
+
+        if (particleSystem2D == null)
+        {
+            Debug.LogError("No ParticleSystem found on this GameObject.");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Auto-destroy when finished
+        if (autoDestroy && particleSystem2D != null && !particleSystem2D.IsAlive())
+        {
+            Destroy(gameObject, destroyDelay);
+        }
+    }
+
+    /// <summary>
+    /// Plays the particle effect.
+    /// </summary>
+    public void PlayEffect()
+    {
+        if (particleSystem2D != null)
+        {
+            particleSystem2D.Play();
+        }
+    }
+
+    /// <summary>
+    /// Stops the particle effect.
+    /// </summary>
+    public void StopEffect()
+    {
+        if (particleSystem2D != null)
+        {
+            particleSystem2D.Stop();
+        }
+    }
+
+    /// <summary>
+    /// Plays the effect at a specific position.
+    /// </summary>
+    public void PlayAtPosition(Vector3 position)
+    {
+        transform.position = position;
+        PlayEffect();
     }
 }
